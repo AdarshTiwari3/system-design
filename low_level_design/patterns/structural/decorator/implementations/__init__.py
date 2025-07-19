ae147fd637523f2e200d738755990d47345d5814
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-07-19 23:51:49 +0530

    WIP

---
+++ low_level_design/patterns/structural/decorator/implementations/__init__.py
@@ -0,0 +1,7 @@
+"""
+Decorator Design Pattern:-
+    1. Decorator is a structural design pattern that lets you attach new behaviors to objects by placing these objects inside special wrapper objects that contain the behaviors.
+    2. Use the Decorator pattern when you need to be able to assign extra behaviors to objects at runtime without breaking the code that uses these objects.
+    3. The Decorator lets you structure your business logic into layers, create a decorator for each layer and compose objects with various combinations of this logic at runtime. The client code can treat all these objects in the same way, since they all follow a common interface.
+    4. Use the pattern when it’s awkward or not possible to extend an object’s behavior using inheritance.
+    """
