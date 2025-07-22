af3417b80b2246b1b2953a8dce6a59ffa708d9c5
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-07-22 10:17:05 +0530

    init of facade design pattern

---
+++ low_level_design/patterns/structural/facade/__init__.py
@@ -0,0 +1,8 @@
+"""Facade Design Pattern:-
+It is used when we have to hide the system complexity from the client.
+or 
+The Facade pattern is used to hide the complexity of a subsystem by providing a simplified interface to the client.
+
+Example: A car — as a driver, you only use the accelerator, brake, and steering. Internally, these controls are connected to complex systems (engine control, braking system, steering mechanism), but the driver does not need to understand or interact with those complexities.
+
+"""
