73fb010977a549d3d9459182f12f9e397bbf7b59
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-07-26 23:52:36 +0530

    wip for flyweight design pattern

---
+++ low_level_design/patterns/structural/flyweight/implementations/__init__.py
@@ -0,0 +1,5 @@
+"""
+The Flyweight Pattern solves this by sharing common state (the intrinsic part) across all similar objects and externalizing unique data (the extrinsic part). It allows you to create lightweight objects by caching and reusing instances instead of duplicating data.
+The Flyweight Pattern minimizes memory usage by sharing as much data as possible between similar objects.
+This is a memory optimization technique
+"""
