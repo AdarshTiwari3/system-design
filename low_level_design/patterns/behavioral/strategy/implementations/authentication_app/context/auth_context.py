0bf93f601e3e68236c016432295670628c07504a
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-08-11 23:55:27 +0530

    strategy design pattern

--- low_level_design/patterns/behavioral/strategy/implementations/authentication_app/context/auth_context.py
+++ low_level_design/patterns/behavioral/strategy/implementations/authentication_app/context/auth_context.py
@@ -1,4 +1,15 @@
 """Implementation of Auth Context"""
 
+from interfaces.auth_strategy import AuthStrategy
 class AuthContext:
-    pass
+    """references the strategies"""
+    def __init__(self, strategy: AuthStrategy) -> None:
+        self.__strategy=strategy
+
+    """sets strategy at run time"""
+    def set_strategy(self, strategy: AuthStrategy):
+        self.__strategy=strategy
+
+
+    def auth(self, user_data: dict[str,str]) -> bool:
+        return self.__strategy.authenticate(user_data)
---
+++ low_level_design/patterns/behavioral/strategy/implementations/authentication_app/main.py
@@ -0,0 +1,8 @@
+"""Main method"""
+from client.client import run_strategy_design
+
+if __name__ == "__main__":
+    print("\nStrategy Design Pattern")
+    run_strategy_design()
+
+    
