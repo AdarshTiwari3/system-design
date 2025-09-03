4343c39fda8231e65828fe97ade0a17a6a0c56fa
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-09-03 23:02:33 +0530

    feat: add some good approaches

--- low_level_design/patterns/behavioral/template_method/shopping_checkout/checkout/base_checkout.py
+++ low_level_design/patterns/behavioral/template_method/shopping_checkout/checkout/base_checkout.py
@@ -7,22 +7,22 @@ class CheckoutProcess(ABC):
     @final
     def _checkout(self,order:dict):
         """Template method (intended as private)"""
-        self.select_item(order)
-        self.add_to_cart(order)
+        self._select_item(order)
+        self._add_to_cart(order)
         self.make_payment(order)
-        self.send_receipt(order)
+        self._send_receipt(order)
 
 
 
-    def select_item(self, order: dict):
+    def _select_item(self, order: dict):
         print(f"\n✔️ Checkout Item Selected={order.get('items')}")
 
-    def add_to_cart(self,order:dict):
+    def _add_to_cart(self,order:dict):
         print(f"\n✔️ Item-{order.get('items')} added to cart 🛒, total={order.get('total')}")
 
     @abstractmethod
     def make_payment(self,order:dict):
         pass
 
-    def send_receipt(self,order:dict):
+    def _send_receipt(self,order:dict):
         print(f"\n✅ Receipt send to {order.get('email')} with order details")
