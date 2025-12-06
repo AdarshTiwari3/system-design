5915a0372f1dc6ba776173fc1251834229a174f5
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-12-06 12:18:25 +0530

    feat: add the feature post, comment, answer and content into stack overflow problem

---
+++ low_level_design/practice_problems/design_stack_overflow/src/answer.py
@@ -0,0 +1,16 @@
+"""Answer is also Post Type"""
+from post import Post
+from user import User
+from uuid import uuid4
+class Answer(Post):
+    def __init__(self, body: str, author: User):
+        super().__init__(str(uuid4()), body, author)
+        self._is_accepted_answer : bool =False
+
+    def set_accepted_answer(self, accepted: bool):
+        self._is_accepted_answer=accepted
+
+    @property
+    def is_accepted_answer(self) -> bool:
+        return self._is_accepted_answer
+    
