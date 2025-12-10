5b335e341f85d19278db322d1826e59edf2b48a7
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-12-10 22:12:18 +0530

    feat: add full code of design logger lld problem with synchronous logging approach

---
+++ low_level_design/practice_problems/design_logger_framework/src/client/client.py
@@ -0,0 +1,38 @@
+"""Client"""
+from log_manager import LogManager
+from enums.log_level import LogLevel
+from appenders.console_log_appender import ConsoleAppender
+from appenders.file_log_appender import FileAppender
+from strategies.json_formatter_strategy import JsonFormatterStrategy
+
+
+
+def main():
+    log_manager = LogManager.get_instance()
+    logger = log_manager.get_logger("payment.service")
+
+    logger.set_level(LogLevel.DEBUG)
+
+   
+    console_text = ConsoleAppender()
+    file_text = FileAppender("logs/payment.log")
+
+    logger.add_appender(console_text)
+    logger.add_appender(file_text)
+
+    logger.debug("Debugging payment flow...")
+    logger.info("Payment request created")
+    logger.warn("High latency detected")
+    logger.error("Payment failed")
+    logger.fatal("Critical failure: shutting down system")
+
+    # --- JSON format appenders ---
+    json_formatter = JsonFormatterStrategy()
+    console_json = ConsoleAppender(json_formatter)
+    file_json = FileAppender("logs/payment.json", json_formatter)
+
+    logger.add_appender(console_json)
+    logger.add_appender(file_json)
+
+    logger.info("Payment started")
+    logger.error("Payment failed due to insufficient balance")
---
+++ low_level_design/practice_problems/design_logger_framework/src/log_manager.py
@@ -0,0 +1,34 @@
+"""LogManager- singleton class"""
+import threading
+from typing import Dict, Self
+from logger import Logger
+class LogManager:
+    _instance: 'LogManager | None'=None
+    _lock=threading.Lock()
+
+    def __init__(self):
+        self.loggers: Dict[str, Logger]={}
+        self.root_logger=Logger('root',None)
+        self.loggers['root']=self.root_logger
+
+    @classmethod
+    def get_instance(cls) -> Self:
+        """Thread safe singleton"""
+        if cls._instance is None:
+            with cls._lock: 
+                if cls._instance is None:
+                    cls._instance=LogManager()
+
+        return cls._instance
+    
+    def get_logger(self, name: str) -> Logger:
+        """Return existing logger or create a new one as a child of root."""
+        if name in self.loggers:
+            return self.loggers[name]
+
+        logger = Logger(name, parent=self.root_logger)
+        self.loggers[name] = logger
+        return logger
+    
+    def get_root_logger(self) -> Logger:
+        return self.root_logger
