dfafa9430217ec3a32a7152e8bf5ff39b941b0f1
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-12-10 17:50:28 +0530

    feat: implement the appenders to append the logs in logger design problem

---
+++ low_level_design/practice_problems/design_logger_framework/src/appenders/console_log_appender.py
@@ -0,0 +1,27 @@
+""" 
+A ConsoleAppender is something that prints logs on the screen (your terminal / console).
+or ConsoleAppender = write logs to the screen.
+Example: 
+
+[INFO] Payment started
+
+"""
+import threading
+from appenders.log_appender_interface import LogAppender
+from strategies.log_formatter_strategy import LogFormatterStrategy
+from log_message import LogMessage
+class ConsoleAppender(LogAppender):
+    _lock = threading.Lock() 
+
+    def append(self, log_message: LogMessage):
+        formatted = self.formatter.format(log_message)
+        with ConsoleAppender._lock:
+            print(formatted)
+    def close(self):
+        pass
+
+    def set_formatter(self, formatter: LogFormatterStrategy):
+        self.formatter=formatter
+
+    def get_formatter(self) -> LogFormatterStrategy:
+        return self.formatter
--- low_level_design/practice_problems/design_logger_framework/src/appenders/log_appender_interface.py
+++ low_level_design/practice_problems/design_logger_framework/src/appenders/log_appender_interface.py
@@ -1,19 +1,26 @@
-"""Log Appender Interface"""
+"""Log Appender Interface- An Appender receives a formatted LogMessage and writes it somewhere — console, file, database, network, etc."""
 from abc import ABC, abstractmethod
 from log_message import LogMessage
+from strategies.log_formatter_strategy import LogFormatterStrategy
+from strategies.text_formatter_strategy import TextFormatterStrategy
+
 class LogAppender(ABC):
+    def __init__(self, formatter: LogFormatterStrategy | None =None):
+        self.formatter= formatter or TextFormatterStrategy()
+
     @abstractmethod
     def append(self, log_message: LogMessage):
         pass
 
     @abstractmethod
     def close(self):
+        # Nothing to close for console
         pass
 
     @abstractmethod
-    def get_formatter(self):
+    def get_formatter(self) -> LogFormatterStrategy:
         pass
 
     @abstractmethod
-    def set_formatter(self, formatter):
+    def set_formatter(self, formatter: LogFormatterStrategy) -> None:
         pass
